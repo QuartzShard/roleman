8bf35a7757f11924223201ea0161b5f63dd59808
quartzshard <matthew.burgess50@gmail.com>
2020-04-22 17:18:41 +0100

    Added more common functionality

--- common.js
+++ common.js
@@ -10,10 +10,51 @@ module.exports = {
      * @param {*} mention The mentioned role from args list
      * Returns an array of [mention, roleID]
      */
-    getRoleIDFromMention(msg, mention) {
+    getRoleIDFromMention(mention) {
         const matches = mention.match(/^<@&(\d+)>$/)
+        if (!matches && mention == "@everyone"){
+            return ["@everyone","@everyone"]
+        } else if (!matches) return
+        
+        return matches
+    },
+    /**
+     * 
+     * @param {*} msg Referrer message 
+     * @param {*} mention The mentioned user from args list
+     * Returns an array of [mention,userID]
+     */
+    getUserIDFromMention(mention) {
+        const matches = mention.match(/^<@!?(\d+)>$/)
         if (!matches) return
         return matches
+    },
+    /**
+     * 
+     * @param {*} mention A discord mention
+     * Returns {type:"role"/"user"/null,mention:mention}
+     */
+    async determineMentionType(mention){
+        if(/^<@&(\d+)>$/.test(mention)|| mention == "@everyone") return {type:"role", mention:mention}
+        if(/^<@!?(\d+)>$/.test(mention))return {type:"user", mention:mention}
+        return {type:null, mention:mention}
+    },
+    /**
+     * 
+     * @param {*} roleID Role to get holders of
+     * @param {*} guild Guild to check in
+     * Returns a collection of all users with a given role in a given guild
+     * If no role if passed, or if @everyone is passed, will return all users
+     */
+    async getUsersFromRole(roleID,guild){
+        const allUsers = await guild.members.fetch()
+        if (roleID && roleID != "@everyone") {
+            const users = await allUsers.filter(u => u.roles.cache.some(r => r.id == roleID));
+            return users
+        } else {
+            return allUsers
+        }
+        
     },
     /**
      * 
@@ -25,26 +66,38 @@ module.exports = {
         if (Array.isArray(roles)) {
             resolvedRoles = []
             for (const role of roles) {
-                resolvedRoles.push(await guild.roles.fetch(role))
+                await resolvedRoles.push(await guild.roles.cache.find(r => r.id == role))
             }
             return resolvedRoles
         } else {
-            return guild.roles.fetch(roles)
+            return await guild.roles.cache.find(r => r.id == roles)
         }
+    },
+    /**
+     * 
+     * @param {*} id User's ID
+     * @param {*} guild Guid to resolve in
+     * Gets a member given a User and a guild
+     */
+    async resolveMemberFromID(id,guild) {
+        const member = await guild.members.cache.find(m => m.user.id = id)
+        return member
     },
     /**
      * 
      * @param {String} title The title of the embed card
      * @param {Array} body The body content of the embed card
      * @param {Boolean} thumbnail Display the thumbnail (default false)
+     * @param {String} url Url to link to
      * Format a message into an embed.
      */
-    embedify(title,body,thumbnail){
+    embedify(title,body,thumbnail,url){
         var embed = new Discord.MessageEmbed()
         .setColor('#b4ebeb')
         .setTimestamp(Date.now())
         
         if (title) embed.setTitle(title)
+        if (url) embed.setURL(url)
         if (body){
             for (const elem of body){
                 embed.addField(hr,elem)
