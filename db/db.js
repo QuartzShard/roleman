d033f1560f8639b884959047115c32ca2af86147
quartzshard <matthew.burgess50@gmail.com>
2020-08-10 13:17:58 +0000

    updated promis library and fixed relative path error

--- db/db.js
+++ db/db.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const mongoose = require('mongoose')
+mongoose.Promise = require('promise')
 const { dbUrl } = require("../config.json")
 
 const mongoCon = mongoose.createConnection(dbUrl, {useNewUrlParser:true})
 mongoCon.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db = new Map()
 
-const modelFiles = fs.readdirSync('./db').filter(file => file.endsWith('.js'));
+const modelFiles = fs.readdirSync(`${__dirname}`).filter(file => file.endsWith('.js'));
 
 for (const file of modelFiles) {
     if (file != "db.js") {
