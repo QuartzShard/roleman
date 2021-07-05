cb6c2ebacd6d7f9aeecd68a766aac22067f76f60
quartzshard <matthew.burgess50@gmail.com>
2021-07-05 22:04:09 +0100

    updated getRole to support numerical ids

--- lib/role.py
+++ lib/role.py
@@ -3,11 +3,14 @@ import discord
 
 ## Get a role objet given a name, mention or id and a guild
 async def getRole(nameOrID, guild):
-    if nameOrID.isdigit():
-        return guild.get_role(int(nameOrID))
-    elif type(nameOrID) is str:
-        for role in guild.roles:
-            if role.name == nameOrID or role.mention == nameOrID:
-                return role
+    if type(nameOrID) is str:
+        if nameOrID.isdigit():
+            return guild.get_role(int(nameOrID))
+        else:
+            for role in guild.roles:
+                if role.name == nameOrID or role.mention == nameOrID:
+                    return role
+    elif type(nameOrID) is int:
+        return guild.get_role(nameOrID)
     else:
         return None
