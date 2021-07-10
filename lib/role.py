4cfe7542eab13bbe17990494bf3660e18f84d48b
quartzshard <matthew.burgess50@gmail.com>
2021-07-10 23:58:47 +0100

    getMember

--- lib/role.py
+++ lib/role.py
@@ -15,3 +15,18 @@ async def getRole(nameOrID, guild):
         return guild.get_role(nameOrID)
     else:
         return None
+
+## Get a member objet given a name, mention or id and a guild
+async def getMember(nameOrID, guild):
+    if type(nameOrID) is str:
+        if nameOrID.isdigit():
+            return guild.get_member(int(nameOrID))
+        else:
+            for member in guild.members:
+                if member.name == nameOrID or str(member.id) in nameOrID:
+                    return member
+            return None
+    elif type(nameOrID) is int:
+        return guild.get_member(nameOrID)
+    else:
+        return None
